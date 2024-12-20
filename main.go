13aa23c8af7b8e083503ac88ffc8077473650eee
alexvitayu <alexvitayu1212@gmail.com>
2024-12-20 18:21:41 +0300

    feat: composition struct

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import (
 	"fmt"
 	"math/rand/v2"
 	"net/url"
+	"time"
 )
 
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!")
+
+// blueprint for struct
 type account struct {
 	url      string
 	login    string
 	password string
 }
 
-func (acc *account) outputInfo() {
-	fmt.Println((acc).url, (acc).login, (acc).password)
+// blueprint for composition struct
+type accountWithTimeStamps struct {
+	createdAt time.Time
+	updatedAt time.Time
+	account
 }
 
+// method struct
+func (acc *accountWithTimeStamps) outputInfo() {
+	fmt.Println(acc.url, acc.login, acc.password, acc.createdAt, (*acc).updatedAt)
+	fmt.Println(*acc)
+}
+
+// method struct
 func (acc *account) generatePassword(n int) {
 	res := make([]rune, n)
 	for i := range res {
@@ -25,38 +39,65 @@ func (acc *account) generatePassword(n int) {
 	acc.password = string(res)
 }
 
-func newAccount(login, password, urlString string) (*account, error) {
+// comosition struct
+func newMyAccountWithTimeStamps(urlString, login, password string) (*accountWithTimeStamps, error) {
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
+	}
 	_, err := url.ParseRequestURI(urlString)
 	if err != nil {
 		return nil, errors.New("INVALID_URL")
 	}
-	return &account{
+	newAcc := &accountWithTimeStamps{
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		account: account{
+			url:      urlString,
+			login:    login,
+			password: password,
+		},
+	}
+	if password == "" {
+		newAcc.account.generatePassword(4)
+	}
+	return newAcc, nil
+}
+
+// constructor struct
+/*func newAccount(urlString, login, password string) (*account, error) {
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
+	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+	anyAcc := &account{
 		url:      urlString,
 		login:    login,
 		password: password,
-	}, nil
-}
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!")
+	}
+	if password == "" {
+		anyAcc.generatePassword(12)
+	}
+	return anyAcc, nil
+}*/
 
 func main() {
-
 	url := addInfo("введите url")
 	login := addInfo("введите логин")
 	password := addInfo("введите пароль")
-	account1, err := newAccount(login, password, url)
+	myAccount, err := newMyAccountWithTimeStamps(url, login, password)
 	if err != nil {
-		fmt.Println("неверный формат URL")
+		fmt.Println("неверный формат URL или логин")
 		return
 	}
-	account1.generatePassword(8)
-
-	account1.outputInfo()
+	myAccount.outputInfo()
 }
 
 func addInfo(data string) string {
-	fmt.Print(data, ": ")
+	fmt.Print(data, ":")
 	var info string
-	fmt.Scan(&info)
+	fmt.Scanln(&info)
 	return info
 }
